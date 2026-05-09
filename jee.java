5cb9b1e724784cd8cd55d5c397a90972eee3190a
Sumith A Shetty <sumithgo17@gmail.com>
2026-05-09 08:35:25 +0000

    v2

--- jee.java
+++ jee.java
@@ -2,12 +2,12 @@ class jee{
     public static void main(String [] args){
         int a[]={4,3,2,5,7,6};
         int cut=0;
-        int j=0;
+        int j=a[0];
         int n=6;
-        for(int i=0;i<n-1;i++){
-            if(a[j]>a[i+1]){
+        for(int i=1;i<n-1;i++){
+            if(a[j]>a[i]){
                 cut+=1;
-                j++;
+                j=a[i];
             }
         }
         System.out.println(cut);
